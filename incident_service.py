329b3da5e5828b32059b4da8e799753431107ce6
santoshrn28 <santosh.rn@gmail.com>
2026-06-03 18:21:00 +0530

    Update incident_service.py

--- incident_service.py
+++ incident_service.py
@@ -1,28 +1,32 @@
-from models import SessionLocal, Incident
+import os
+import uuid
+from datetime import datetime, timezone
+import boto3
+
+AWS_REGION = os.getenv("AWS_REGION", "ap-south-1")
+INCIDENT_TABLE = os.getenv("INCIDENT_TABLE", "support-incidents")
+
+dynamodb = boto3.resource("dynamodb", region_name=AWS_REGION)
+table = dynamodb.Table(INCIDENT_TABLE)
 
 def create_incident(data: dict):
-    db = SessionLocal()
-    try:
-        incident = Incident(
-            customer_name=data["customer_name"],
-            email=data["email"],
-            title=data["title"],
-            description=data["description"],
-            category=data.get("category"),
-            severity=data.get("severity", "Medium"),
-            status="Open",
-        )
-        db.add(incident)
-        db.commit()
-        db.refresh(incident)
-        return incident
-    finally:
-        db.close()
+    incident_id = f"INC-{uuid.uuid4().hex[:8].upper()}"
+
+    item = {
+        "incident_id": incident_id,
+        "customer_name": data["customer_name"],
+        "email": data["email"],
+        "title": data["title"],
+        "description": data["description"],
+        "category": data.get("category", "General"),
+        "severity": data.get("severity", "Medium"),
+        "status": "Open",
+        "created_at": datetime.now(timezone.utc).isoformat()
+    }
+
+    table.put_item(Item=item)
+    return item
 
 def list_incidents():
-    db = SessionLocal()
-    try:
-        return db.query(Incident).all()
-    finally:
-        db.close()
-``
+    response = table.scan()
+    return response.get("Items", [])
